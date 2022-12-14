13516bc530f243d2f2cbb6519556563d34cf7747
hoanghongdang <56016856+hoanghongdang@users.noreply.github.com>
2022-12-14 14:19:21 +0700

    demo c sharp

---
+++ Demo1Github/sweatkey/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+Console.InputEncoding = Encoding.Unicode;
+Console.OutputEncoding= Encoding.Unicode;
+
+int number1 = Random.Shared.Next(0,9);
+int number2 = Random.Shared.Next(0,9);
+Console.WriteLine($"{nameof(number1)} = {number1}");
+Console.WriteLine($"{nameof(number2)} = {number2}");
+
+Console.WriteLine("vui lòng nhập dấu: ");
+var pheptoan = Console.ReadLine();
+switch (pheptoan)
+{
+    case "+" when number1 > 0 && number2 > 0: 
+        Console.WriteLine(number1 + number2); 
+        break;
+    case "-" when number1 >= number2:
+        Console.WriteLine(number1 - number2);
+        break;
+    case "*":
+        Console.WriteLine(number1 * number2);
+        break;
+    case "/" when number2 != 0:
+        Console.WriteLine(number1 / number2);
+        break;
+    default:
+        Console.WriteLine("..............");
+        break;
+}
+
+Console.WriteLine(
+    pheptoan switch
+        {
+            "+" when number1 > 0 && number2 > 0 => number1 + number2,
+            "-" when number1 >= number2 => number1 - number2,
+            "*" => number1 * number2,
+            "/" when number2 != 0 => number1 / number2
+        }
+    ) ;
