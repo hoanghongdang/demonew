337e9ac2d0aaeca1bcd18f63c415eadd84cb0512
hoanghongdang <56016856+hoanghongdang@users.noreply.github.com>
2022-12-14 15:05:37 +0700

    demo c sharp

--- Demo1Github/break/Program.cs
+++ Demo1Github/break/Program.cs
@@ -3,11 +3,15 @@ using System.Text;
 
 Console.InputEncoding = Encoding.Unicode;
 Console.OutputEncoding = Encoding.Unicode;
-
+/*
 int a = 10;
-int b = 5;
-
+int b = 5;*/
+int a;
+int b;
 //ref a = a ; b = b
 //bỏ ref a = b; b = a
-newrefout.ChangeNumber(ref a,ref b);
+/*newrefout.ChangeNumber(ref a,ref b);
+Console.WriteLine($"Program : {nameof(a)}={a},{nameof(b)}={b}");*/
+newrefout.ChangeNumber(out a, out b);
 Console.WriteLine($"Program : {nameof(a)}={a},{nameof(b)}={b}");
+
--- Demo1Github/break/newrefout.cs
+++ Demo1Github/break/newrefout.cs
@@ -11,11 +11,20 @@ internal class newrefout
     //khi 1 phương thức chỉ có 1 câu lệnh duy nhất
     public void ShowInfo() => Console.WriteLine("thông tin về class");
 
-    public static void ChangeNumber(ref int a,ref int b)
+    /* public static void ChangeNumber(ref int a,ref int b)
+     {
+         int tam = a;
+         a= b;
+         b= tam;
+         Console.WriteLine($"ChangeNumber : {nameof(a)}={a},{nameof(b)}={b}");
+     }*/
+    public static void ChangeNumber(out int a, out int b)
     {
+        a = 10;
+        b = 5;
         int tam = a;
-        a= b;
-        b= tam;
+        a = b;
+        b = tam;
         Console.WriteLine($"ChangeNumber : {nameof(a)}={a},{nameof(b)}={b}");
     }
 
