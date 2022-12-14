44b582d9274739b18eeb456ef9175bd43241327e
hoanghongdang <56016856+hoanghongdang@users.noreply.github.com>
2022-12-14 13:29:00 +0700

    demo c sharp

--- Demo1Github/Demo1Github/Program.cs
+++ Demo1Github/Demo1Github/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double a = 0.1;
+double b = 0.2;
+Console.WriteLine(a + b == 0.3 ? "true":"false");
+
+
+float c = 0.1F;//float phai co F
+decimal d = 0.2M;//decimal phai co M
+decimal e = 0.1M;
+Console.WriteLine(e+d == 0.3M ? "true" : "false");
