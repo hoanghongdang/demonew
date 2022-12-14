036b7ecf50ea030ee54ede286177b5aeb874dbcc
hoanghongdang <56016856+hoanghongdang@users.noreply.github.com>
2022-12-14 13:35:32 +0700

    demo c sharp

--- Demo1Github/Demo1Github/Program.cs
+++ Demo1Github/Demo1Github/Program.cs
@@ -1,9 +1,15 @@
 ﻿double a = 0.1;
 double b = 0.2;
-Console.WriteLine(a + b == 0.3 ? "true":"false");
+Console.WriteLine(a + b == 0.3 ? "true" : "false");
 
 
 float c = 0.1F;//float phai co F
 decimal d = 0.2M;//decimal phai co M
 decimal e = 0.1M;
 Console.WriteLine(e+d == 0.3M ? "true" : "false");
+
+//?: ==> ternary operator
+
+string fullname = null;
+//int i = null;
+Nullable<int> i = null;
