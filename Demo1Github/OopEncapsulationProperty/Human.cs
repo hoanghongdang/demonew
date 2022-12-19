e643cc72d793ea3fddf7f866695488f10838fb14
hoanghongdang <56016856+hoanghongdang@users.noreply.github.com>
2022-12-19 13:52:21 +0700

    demo c sharp

---
+++ Demo1Github/OopEncapsulationProperty/Human.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopEncapsulationProperty;
+internal class Human
+{
+    //fields các field phải là private
+    private string fullname;
+    //thuộc tính address
+    public string Address { get; set; }
+    public int Age { get; set; }
+
+    //các phương thức getter, setter (cách cho mobie)
+    
+    public string Fullname {
+        set => fullname = value; 
+        
+        get => fullname;
+        
+    }
+
+    public void ShowPeopel()
+    {
+        Console.WriteLine($"{nameof(Address)}={Address}");
+        Console.WriteLine($"{nameof(fullname)}={fullname}");
+    }
+}
---
+++ Demo1Github/OopPolymorphism/Program.cs
@@ -0,0 +1,11 @@
+﻿using OopPolymorphism;
+
+BasicMath basicMath1 = new();
+BasicMath basicMath2 = new()
+{
+    NumberOne= 1,
+    NumberTwo= 2
+};
+BasicMath basicMath3 = new(5);
+BasicMath basicMath4 = new(numberTwo:5);
+
