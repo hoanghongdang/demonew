11d5c43ce4c73add4d160ef7b6c98f159076478c
hoanghongdang <56016856+hoanghongdang@users.noreply.github.com>
2022-12-19 14:32:42 +0700

    demo c sharp

--- Demo1Github/OopPolymorphsmInterface/Program.cs
+++ Demo1Github/OopPolymorphsmInterface/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OopPolymorphsmInterface;
+
+FileOne fileOne = new();
+fileOne.WriteBinaryFile();
+fileOne.WriteTestFile();
+fileOne.ReadFile();
+
+//cách cũ
+((IBinaryFile)fileOne).ShowInfo();
+(fileOne as IBinaryFile).ShowInfo();
+
+FileTwo fileTwo = new();
+fileTwo.WriteBinaryFile();
+fileTwo.WriteTestFile();
+(fileTwo as ITestFile).ReadFile();
+(fileTwo as IBinaryFile).ReadFile();
